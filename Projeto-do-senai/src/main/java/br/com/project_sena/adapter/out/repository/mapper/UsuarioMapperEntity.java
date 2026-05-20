af7b14eb9f39136cb87ea8f1e42a0495a68afc01
rthurlucas <arthurlucasx696@gmail.com>
2026-05-19 21:30:10 -0300

    feat: Create the class Mapper for to do contact with entity and domain

--- Projeto-do-senai/src/main/java/br/com/project_sena/adapter/out/repository/mapper/UsuarioMapperEntity.java
+++ Projeto-do-senai/src/main/java/br/com/project_sena/adapter/out/repository/mapper/UsuarioMapperEntity.java
@@ -1,7 +1,30 @@
 package br.com.project_sena.adapter.out.repository.mapper;
 
+import br.com.project_sena.adapter.out.repository.entity.UsuarioEntity;
+import br.com.project_sena.application.core.domain.model.Usuario;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UsuarioMapperEntity {
+    public Usuario toDomain(UsuarioEntity entity){
+        return new Usuario(
+                entity.getId(),
+                entity.getName(),
+                entity.getPassword(),
+                entity.getLogin(),
+                entity.isActive(),
+                entity.getPerfil()
+        );
+    }
+
+    public UsuarioEntity toEntity(Usuario usuario){
+        return new UsuarioEntity(
+                usuario.getId(),
+                usuario.getName(),
+                usuario.getPassword(),
+                usuario.getLogin(),
+                usuario.isActive(),
+                usuario.getPerfil()
+        );
+    }
 }
