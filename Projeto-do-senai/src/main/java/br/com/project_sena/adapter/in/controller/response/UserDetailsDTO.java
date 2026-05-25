ffcefe469e442d7e327155f767970055800b9d4b
rthurlucas <arthulucasx696@gmail.com>
2026-05-25 15:57:58 -0300

    feat: add usuarioEnum field to UserDetailsDTO for improved user role representation

--- Projeto-do-senai/src/main/java/br/com/project_sena/adapter/in/controller/response/UserDetailsDTO.java
+++ Projeto-do-senai/src/main/java/br/com/project_sena/adapter/in/controller/response/UserDetailsDTO.java
@@ -1,11 +1,13 @@
 package br.com.project_sena.adapter.in.controller.response;
 
 import br.com.project_sena.application.core.domain.enums.PerfilEnum;
+import br.com.project_sena.application.core.domain.enums.UsuarioEnum;
 
 public record UserDetailsDTO(
         Long userId,
         String name,
         String login,
-        PerfilEnum perfil
+        PerfilEnum perfil,
+        UsuarioEnum usuarioEnum
 ){
 }
