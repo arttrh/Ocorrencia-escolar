18624f11005124e7817dc55dfcca6f880956922d
rthurlucas <arthulucasx696@gmail.com>
2026-05-25 15:57:22 -0300

    feat: add perfil field to UserUpdateDTO for user role management

--- Projeto-do-senai/src/main/java/br/com/project_sena/adapter/in/controller/request/UserUpdateDTO.java
+++ Projeto-do-senai/src/main/java/br/com/project_sena/adapter/in/controller/request/UserUpdateDTO.java
@@ -1,6 +1,9 @@
 package br.com.project_sena.adapter.in.controller.request;
 
+import br.com.project_sena.application.core.domain.enums.PerfilEnum;
+
 public record UserUpdateDTO(
-        String password
+        String password,
+        PerfilEnum perfil
 ) {
 }
