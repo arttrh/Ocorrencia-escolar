bea94d23d63936daa511bb1c8ebd628d6ac508c8
rthurlucas <arthulucasx696@gmail.com>
2026-05-25 16:07:03 -0300

    feat: add global error handler for UsuarioNotFoundException

--- Projeto-do-senai/src/main/java/br/com/project_sena/exception/GlobalHandlerErrors.java
+++ Projeto-do-senai/src/main/java/br/com/project_sena/exception/GlobalHandlerErrors.java
@@ -1,4 +1,17 @@
 package br.com.project_sena.exception;
 
+import br.com.project_sena.exception.type.UsuarioNotFoundException;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+// Classe Global de tratar Erros
+@Configuration
 public class GlobalHandlerErrors {
+
+    @ExceptionHandler(UsuarioNotFoundException.class)
+    public String handleUsuarioNotFoundException(UsuarioNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+
 }
