e391870294e4b12defff81901cd92f3360574cf7
rthurlucas <arthulucasx696@gmail.com>
2026-05-25 15:57:47 -0300

    feat: update UserListDTO and UserRegisterDTO to include UsuarioEnum for enhanced user role management

--- Projeto-do-senai/src/main/java/br/com/project_sena/adapter/in/controller/request/UserRegisterDTO.java
+++ Projeto-do-senai/src/main/java/br/com/project_sena/adapter/in/controller/request/UserRegisterDTO.java
@@ -1,11 +1,13 @@
 package br.com.project_sena.adapter.in.controller.request;
 
 import br.com.project_sena.application.core.domain.enums.PerfilEnum;
+import br.com.project_sena.application.core.domain.enums.UsuarioEnum;
 
 public record UserRegisterDTO(
         String name,
         String password,
         String login,
-        PerfilEnum perfil
+        PerfilEnum perfi,
+        UsuarioEnum usuarioEnum
 ) {
 }
--- Projeto-do-senai/src/main/java/br/com/project_sena/adapter/in/controller/response/UserListDTO.java
+++ Projeto-do-senai/src/main/java/br/com/project_sena/adapter/in/controller/response/UserListDTO.java
@@ -1,12 +1,12 @@
 package br.com.project_sena.adapter.in.controller.response;
 
 import br.com.project_sena.application.core.domain.enums.PerfilEnum;
-import br.com.project_sena.application.core.domain.model.Usuario;
+import br.com.project_sena.application.core.domain.enums.UsuarioEnum;
 
 public record UserListDTO(
         Long id,
         String name,
-        boolean active,
-        PerfilEnum perfil
+        PerfilEnum perfil,
+        UsuarioEnum usuarioEnum
 ) {
 }
