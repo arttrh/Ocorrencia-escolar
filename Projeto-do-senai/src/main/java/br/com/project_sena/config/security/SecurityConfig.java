0b46ae3572dac3fe1263fe99e12d12b18ff16964
rthurlucas <arthulucasx696@gmail.com>
2026-05-27 15:09:05 -0300

    fix: doing the authorization part in Spring security

--- Projeto-do-senai/src/main/java/br/com/project_sena/config/security/SecurityConfig.java
+++ Projeto-do-senai/src/main/java/br/com/project_sena/config/security/SecurityConfig.java
@@ -2,15 +2,25 @@ package br.com.project_sena.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/usuario/cadastrar", "/login").permitAll().anyRequest().authenticated())
+                .build();
     }
 }
