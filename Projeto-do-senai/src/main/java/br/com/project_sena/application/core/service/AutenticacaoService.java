47e502f08f4c3866a48861405700a2adfcd7d4c0
rthurlucas <arthulucasx696@gmail.com>
2026-05-28 18:45:03 -0300

    fix: correcting the logic in the log method and putting try and catch

--- Projeto-do-senai/src/main/java/br/com/project_sena/application/core/service/AutenticacaoService.java
+++ Projeto-do-senai/src/main/java/br/com/project_sena/application/core/service/AutenticacaoService.java
@@ -1,35 +1,48 @@
 package br.com.project_sena.application.core.service;
 
 import br.com.project_sena.adapter.in.controller.request.LoginDTO;
+import br.com.project_sena.application.core.domain.model.Usuario;
 import br.com.project_sena.application.port.out.UsuarioRepository;
+import br.com.project_sena.config.security.service.TokenDTO;
+import br.com.project_sena.config.security.service.TokenService;
 import br.com.project_sena.exception.type.SenhaException;
+import br.com.project_sena.exception.type.UsuarioNotFoundException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.security.auth.login.LoginException;
+
 @Service
 public class AutenticacaoService implements UserDetailsService {
     private final UsuarioRepository usuarioRepository;
     private final PasswordEncoder encoder;
+    private final TokenService service;
 
-    public AutenticacaoService(UsuarioRepository usuarioRepository, PasswordEncoder encoder) {
+    public AutenticacaoService(UsuarioRepository usuarioRepository, PasswordEncoder encoder, TokenService service) {
         this.usuarioRepository = usuarioRepository;
         this.encoder = encoder;
+        this.service = service;
     }
 
     @Override
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
-        return usuarioRepository.loadUserByUsername(login);
+        return usuarioRepository.fidByLogin(login);
     }
 
-    public void logar(LoginDTO dto) {
-        UserDetails usuario = usuarioRepository.loadUserByUsername(dto.login());
-        boolean senhaCorreta = encoder.matches(dto.password(), usuario.getPassword());
+    public TokenDTO logar(LoginDTO dto) throws LoginException {
+        try {
+            Usuario usuario = usuarioRepository.fidByLogin(dto.login());
+            boolean senhaCorreta = encoder.matches(dto.password(), usuario.getPassword());
+            if (!senhaCorreta) {
+                throw new SenhaException("Senha invalida por favor digite novamente");
+            }
+            return service.gerarToken(usuario);
 
-        if (!senhaCorreta) {
-            throw new SenhaException("Senha invalida por favor digite novamente");
+        } catch (UsuarioNotFoundException ex) {
+            throw new LoginException("Usuario Nao encontrado por esse Login");
         }
     }
 }
