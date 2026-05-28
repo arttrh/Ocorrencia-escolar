b3b10ed4a9369847b940c810d37c53dc443c042a
rthurlucas <arthulucasx696@gmail.com>
2026-05-28 15:20:40 -0300

    feat: created TokenService for generated TokenJWT and checking login in AutenticacaoService

--- Projeto-do-senai/src/main/java/br/com/project_sena/application/core/service/AutenticacaoService.java
+++ Projeto-do-senai/src/main/java/br/com/project_sena/application/core/service/AutenticacaoService.java
@@ -1,21 +1,35 @@
 package br.com.project_sena.application.core.service;
 
+import br.com.project_sena.adapter.in.controller.request.LoginDTO;
 import br.com.project_sena.application.port.out.UsuarioRepository;
+import br.com.project_sena.exception.type.SenhaException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AutenticacaoService implements UserDetailsService {
-    private UsuarioRepository usuarioRepository;
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder encoder;
 
-    public AutenticacaoService(UsuarioRepository usuarioRepository) {
+    public AutenticacaoService(UsuarioRepository usuarioRepository, PasswordEncoder encoder) {
         this.usuarioRepository = usuarioRepository;
+        this.encoder = encoder;
     }
 
     @Override
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
         return usuarioRepository.loadUserByUsername(login);
+    }
+
+    public void logar(LoginDTO dto) {
+        UserDetails usuario = usuarioRepository.loadUserByUsername(dto.login());
+        boolean senhaCorreta = encoder.matches(dto.password(), usuario.getPassword());
+
+        if (!senhaCorreta) {
+            throw new SenhaException("Senha invalida por favor digite novamente");
+        }
     }
 }
--- Projeto-do-senai/src/main/java/br/com/project_sena/config/security/service/TokenService.java
+++ Projeto-do-senai/src/main/java/br/com/project_sena/config/security/service/TokenService.java
@@ -1,4 +1,27 @@
 package br.com.project_sena.config.security.service;
 
+import br.com.project_sena.application.core.domain.model.Usuario;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TokenService {
+
+    private String secret = "{JWT_SECRET}";
+
+    public String gerarToken(Usuario usuario) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("Sistema-Ocorrencia-Escolar")
+                    .withSubject(usuario.getLogin())
+                    .withClaim("id", usuario.getId())
+                    .withClaim("Perfil", usuario.getPerfil().name())
+                    .sign(algorithm);
+            return token;
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
