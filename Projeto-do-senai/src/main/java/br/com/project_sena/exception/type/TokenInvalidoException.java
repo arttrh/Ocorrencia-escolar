29ca386811283e9cfef881e43d515afcf91ee70e
rthurlucas <arthurlucasx696@gmail.com>
2026-05-29 19:49:05 -0300

    feat: class exception

---
+++ Projeto-do-senai/src/main/java/br/com/project_sena/exception/type/TokenInvalidoException.java
@@ -0,0 +1,7 @@
+package br.com.project_sena.exception.type;
+
+public class TokenInvalidoException extends RuntimeException {
+    public TokenInvalidoException(String message) {
+        super(message);
+    }
+}
