f719053074887a797e1d20de3f90d310bb3c4769
rthurlucas <arthulucasx696@gmail.com>
2026-06-01 16:26:01 -0300

    feat: update student DTOs to include AlunoEnum and rename StudentListDTO to StudentListAtivosDTO

--- Projeto-do-senai/src/main/java/br/com/project_sena/adapter/in/controller/response/StudentListInativosDTO.java
+++ Projeto-do-senai/src/main/java/br/com/project_sena/adapter/in/controller/response/StudentListInativosDTO.java
@@ -1,15 +1,14 @@
 package br.com.project_sena.adapter.in.controller.response;
 
 import br.com.project_sena.application.core.domain.enums.AlunoEnum;
-import br.com.project_sena.application.core.domain.model.Aluno;
 
 import java.time.LocalDate;
 
-public record StudentDetailsDTO(
-        Long studentId,
+public record StudentListInativosDTO(
+        Long id,
         String photo,
         String name,
         LocalDate dateBirth,
         AlunoEnum alunoEnum
-){
+) {
 }
