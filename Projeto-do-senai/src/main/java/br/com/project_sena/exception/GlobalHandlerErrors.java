331582c514241b42e6f6475dfe2be50067c64017
rthurlucas <arthurlucasx696@gmail.com>
2026-06-01 21:32:13 -0300

    feat: enhance GlobalHandlerErrors to handle additional exceptions with appropriate HTTP responses

--- Projeto-do-senai/src/main/java/br/com/project_sena/exception/GlobalHandlerErrors.java
+++ Projeto-do-senai/src/main/java/br/com/project_sena/exception/GlobalHandlerErrors.java
@@ -1,7 +1,12 @@
 package br.com.project_sena.exception;
 
+import br.com.project_sena.exception.type.EmailException;
+import br.com.project_sena.exception.type.SenhaException;
+import br.com.project_sena.exception.type.TokenInvalidoException;
 import br.com.project_sena.exception.type.UsuarioNotFoundException;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,9 +16,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalHandlerErrors {
 
     @ExceptionHandler(UsuarioNotFoundException.class)
-    public String handleUsuarioNotFoundException(UsuarioNotFoundException ex) {
-        return ex.getMessage();
+    public ResponseEntity<String> handleUsuarioNotFoundException(UsuarioNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
+    @ExceptionHandler(SenhaException.class)
+    public ResponseEntity<String> handlerSenha(SenhaException ex){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(EmailException.class)
+    public ResponseEntity<String> handlerEmail(EmailException ex){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(TokenInvalidoException.class)
+    public ResponseEntity<String> handlerTokenInvalido(TokenInvalidoException ex){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
 
 }
