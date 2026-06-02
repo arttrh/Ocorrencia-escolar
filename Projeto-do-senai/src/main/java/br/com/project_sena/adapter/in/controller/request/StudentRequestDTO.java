c9f7a65de630bf528f19a4a4598ee5f89519d48f
rthurlucas <arthulucasx696@gmail.com>
2026-06-02 16:57:23 -0300

    feat: add JSON date format annotation to dateBirth field in StudentRequestDTO

--- Projeto-do-senai/src/main/java/br/com/project_sena/adapter/in/controller/request/StudentRequestDTO.java
+++ Projeto-do-senai/src/main/java/br/com/project_sena/adapter/in/controller/request/StudentRequestDTO.java
@@ -1,7 +1,9 @@
 package br.com.project_sena.adapter.in.controller.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -11,6 +13,7 @@ public record StudentRequestDTO(
         @NotBlank
         String name,
         @NotNull
+        @JsonFormat(pattern = "dd/MM/yyyy")
         LocalDate dateBirth
 ) {
 }
