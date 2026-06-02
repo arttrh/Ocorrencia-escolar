1e5c2f55f66e6ea70cd589b2eac2e4035d3450d1
rthurlucas <arthulucasx696@gmail.com>
2026-06-02 16:58:55 -0300

    feat: integrate rate limiting in login process with IP tracking

--- Projeto-do-senai/src/main/java/br/com/project_sena/adapter/in/controller/LoginController.java
+++ Projeto-do-senai/src/main/java/br/com/project_sena/adapter/in/controller/LoginController.java
@@ -2,8 +2,12 @@ package br.com.project_sena.adapter.in.controller;
 
 import br.com.project_sena.adapter.in.controller.request.EmailDTO;
 import br.com.project_sena.application.core.service.AutenticacaoService;
+import br.com.project_sena.config.security.rateLimit.RateLimitService;
 import br.com.project_sena.config.security.service.TokenDTO;
 import br.com.project_sena.config.security.service.TokenService;
+import io.github.bucket4j.Bucket;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,18 +16,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.security.auth.login.LoginException;
 
+@Slf4j
 @RestController
 @RequestMapping("/login")
 public class LoginController {
 
+    private final RateLimitService serviceRate;
     private final AutenticacaoService service;
 
-    public LoginController(AutenticacaoService service, TokenService token) {
+    public LoginController(AutenticacaoService service, TokenService token, RateLimitService serviceRate) {
         this.service = service;
+        this.serviceRate = serviceRate;
     }
 
     @PostMapping
-    public ResponseEntity<TokenDTO> logar(@RequestBody EmailDTO dto) throws LoginException {
+    public ResponseEntity<TokenDTO> logar(
+            @RequestBody EmailDTO dto,
+            HttpServletRequest request)
+            throws LoginException {
+        String ip = request.getRemoteAddr(); //Endereco de ip para quem esta acessando
+        log.info("ip: {}", ip);
+        Bucket bucket = serviceRate.getBucket(ip);
+
         TokenDTO usuario = service.logar(dto);
         return ResponseEntity.ok(usuario);
     }
