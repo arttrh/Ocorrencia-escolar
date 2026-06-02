733deb6463f601f6ceae284f7fa9ff516bd1b9eb
rthurlucas <arthulucasx696@gmail.com>
2026-06-02 16:56:53 -0300

    feat: implement rate limiting for login requests with configurable bucket settings

---
+++ Projeto-do-senai/src/main/java/br/com/project_sena/config/security/rateLimit/RateLimitService.java
@@ -0,0 +1,28 @@
+package br.com.project_sena.config.security.rateLimit;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class RateLimitService {
+
+    private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
+
+    public Bucket getBucket(String ip){
+        return buckets.computeIfAbsent(ip, k -> {
+            Bandwidth limit = Bandwidth.classic(
+                    5,
+                    Refill.greedy(5, Duration.ofMinutes(1))
+            );
+            return Bucket.builder()
+                    .addLimit(limit)
+                    .build();
+        });
+    }
+}
