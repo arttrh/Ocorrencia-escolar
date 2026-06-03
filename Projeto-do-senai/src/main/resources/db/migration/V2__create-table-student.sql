8e08ebb3062dee2af446b1f0e621e5c2741fdeab
rthurlucas <arthurlucasx696@gmail.com>
2026-06-02 21:20:00 -0300

    feat: add foreign key constraint for class in student table

--- Projeto-do-senai/src/main/resources/db/migration/V2__create-table-student.sql
+++ Projeto-do-senai/src/main/resources/db/migration/V2__create-table-student.sql
@@ -1,7 +1,12 @@
 CREATE TABLE student(
     id_student BIGSERIAL PRIMARY KEY,
+    id_class BIGINT NOT NULL,
     photo TEXT NOT NULL,
     name VARCHAR(100) NOT NULL,
     date_birth DATE NOT NULL,
-    aluno_enum VARCHAR(50) NOT NULL
+    aluno_enum VARCHAR(50) NOT NULL,
+
+    CONSTRAINT fk_student_class
+    FOREIGN KEY (id_class)
+    REFERENCES class(id_class)
 );
