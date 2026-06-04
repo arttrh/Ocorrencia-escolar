3e16c25f1dd09b0eead7d691cfa82d6c82ac6298
rthurlucas <arthulucasx696@gmail.com>
2026-06-04 17:36:51 -0300

    feat: remove id_class from student table and related foreign key constraint

--- Projeto-do-senai/src/main/resources/db/migration/V2__create-table-student.sql
+++ Projeto-do-senai/src/main/resources/db/migration/V2__create-table-student.sql
@@ -1,12 +1,7 @@
 CREATE TABLE student(
     id_student BIGSERIAL PRIMARY KEY,
-    id_class BIGINT NOT NULL,
     photo TEXT NOT NULL,
     name VARCHAR(100) NOT NULL,
     date_birth DATE NOT NULL,
-    aluno_enum VARCHAR(50) NOT NULL,
-
-    CONSTRAINT fk_student_class
-    FOREIGN KEY (id_class)
-    REFERENCES class(id_class)
+    aluno_enum VARCHAR(50) NOT NULL
 );
