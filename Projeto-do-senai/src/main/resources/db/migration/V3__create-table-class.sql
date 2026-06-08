7597dc6910c2d8b03a162d91287e3218fb621074
rthurlucas <arthulucasx696@gmail.com>
2026-06-08 13:54:20 -0300

    refactor: change class_year and semestry columns to TIMESTAMP in class table

--- Projeto-do-senai/src/main/resources/db/migration/V3__create-table-class.sql
+++ Projeto-do-senai/src/main/resources/db/migration/V3__create-table-class.sql
@@ -1,8 +1,8 @@
 CREATE TABLE class(
     id_class BIGSERIAL PRIMARY KEY,
     class_name VARCHAR(50) NOT NULL,
-    class_year INT NOT NULL CHECK(class_year >= 2000 AND class_year <= 2100),
+    class_year TIMESTAMP NOT NULL,
     turma_enum VARCHAR(100),
     turma_turno VARCHAR(50),
-    semestry DATE NOT NULL
+    semestry TIMESTAMP NOT NULL
 );
